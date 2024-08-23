75a921851a9848e03119aca5e1013dec345343dd
yoyoyoyo0014 <yoyoyoyo0014@gmail.com>
2024-08-23 15:09:57 +0900

    folder

--- TeamProject/src/main/java/kr/kh/app/controller/Main.java
+++ TeamProject/src/main/java/kr/kh/app/controller/Main.java
@@ -1,6 +1,7 @@
 package kr.kh.app.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -12,8 +13,9 @@ import javax.servlet.http.HttpServletResponse;
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-  
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, 
+			HttpServletResponse response) 
+		throws ServletException, IOException {
 		request.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(request, response);
 	}
 
