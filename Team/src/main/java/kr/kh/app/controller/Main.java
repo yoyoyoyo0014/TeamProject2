3f2fcd4a0967e869acc651a84d8f2266fc77137a
suvin5027 <123538283+suvin5027@users.noreply.github.com>
2024-08-26 17:46:31 +0900

    [psv] 경로 수정 업데이트
    [psv] 경로 수정 업데이트

--- Team/src/main/java/kr/kh/app/controller/Main.java
+++ Team/src/main/java/kr/kh/app/controller/Main.java
@@ -16,7 +16,7 @@ public class Main extends HttpServlet {
 	protected void doGet(HttpServletRequest request, 
 			HttpServletResponse response) 
 		throws ServletException, IOException {
-		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
+		request.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(request, response);
 	}
 
 }
