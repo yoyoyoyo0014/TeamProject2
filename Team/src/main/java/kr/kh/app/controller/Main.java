aea1cb10046750abd20daada9e7edb26e0119675
limwoochul <wclim0319@naver.com>
2024-08-26 17:50:32 +0900

    메인 navbar

--- Team/src/main/java/kr/kh/app/controller/Main.java
+++ Team/src/main/java/kr/kh/app/controller/Main.java
@@ -13,9 +13,8 @@ import javax.servlet.http.HttpServletResponse;
 public class Main extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	protected void doGet(HttpServletRequest request, 
-			HttpServletResponse response) 
-		throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
 		request.getRequestDispatcher("/WEB-INF/views/main.jsp").forward(request, response);
 	}
 
--- Team/src/main/webapp/WEB-INF/views/main.jsp
+++ Team/src/main/webapp/WEB-INF/views/main.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +8,15 @@
 <title>Insert title here</title>
 </head>
 <body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+	<ul class="navbar-nav">
+		<li class="nav-item">
+			<a class="navbar-brand" href="<c:url value="/"/>">Home</a>
+		</li>
 
+	</ul>
+	
+
+</nav>
 </body>
 </html>
