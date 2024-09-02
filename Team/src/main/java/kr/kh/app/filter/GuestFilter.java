7474b507eb5c7d55dce2a91008c890c214209cbb
limwoochul <wclim0319@naver.com>
2024-09-02 17:51:55 +0900

    멤버 필터링 구현완료

---
+++ Team/src/main/java/kr/kh/app/filter/GuestFilter.java
@@ -0,0 +1,36 @@
+package kr.kh.app.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import kr.kh.app.model.vo.MemberVO;
+
+@WebFilter("/login")
+public class GuestFilter extends HttpFilter implements Filter {
+       
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest hrequest = (HttpServletRequest)request;
+		HttpSession session = hrequest.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		
+		if(user != null) {
+			request.setAttribute("msg",	"로그인한 사용자는 이용할 수 없는 서비스입니다.");
+			request.setAttribute("url", "/main");
+			request.getRequestDispatcher("/WEB-INF/views/message.jsp").forward(request, response);
+			return;
+		}
+		chain.doFilter(request, response);
+		
+	}
+
+
+}
