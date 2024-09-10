fec19fc1fceb7d2dc3875f89f8a36c90b453e6bc
HU2232 <sujifi@naver.com>
2024-09-10 17:15:45 +0900

    .

--- Team/src/main/java/kr/kh/app/service/SubjectService.java
+++ Team/src/main/java/kr/kh/app/service/SubjectService.java
@@ -5,7 +5,28 @@ import java.util.List;
 import kr.kh.app.model.vo.SubjectVO;
 
 public interface SubjectService {
+<<<<<<< Updated upstream
 
 	List<SubjectVO> getSubjectList();
+=======
+	
+	List<SubjectVO> getSubjectStatusList();
+
+	List<SubjectVO> subjectSuMaNumList();
+
+	boolean subjectInsert(SubjectVO subject);
+
+	List<SubjectVO> getSubjectLectureList();
+
+	boolean professorSubjectInsert(LectureVO lecture);
+
+	PageMaker getPageMaker(Criteria cri, int displayPageNum);
+
+	List<SubjectVO> getSubjectList(Criteria cri);
+
+	List<SubjectVO> subjectList();
+
+	List<MemberVO> professorList();
+>>>>>>> Stashed changes
 
 }
